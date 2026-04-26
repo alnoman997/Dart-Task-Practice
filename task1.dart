9ee3449c63ecc8baea0d1e8c2ce4aa4071155fc8
alnoman997 <alnoman997@gmail.com>
2026-04-26 20:10:33 +0600

    first commit

---
+++ task1.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+void main() {
+  print("Enter a number : ");
+
+  try {
+    int num = int.parse(stdin.readLineSync()!);
+
+    if (num % 2 == 0) {
+      print("$num is an even number");
+    } else {
+      print("$num is an odd number");
+    }
+
+    if (num > 0) {
+      print("$num is a positive number");
+    } else if (num < 0) {
+      print("$num is a negative number");
+    } else {
+      print("$num is zero");
+    }
+  } catch (e) {
+    print("Invalid input. Please enter a valid integer.");
+  }
+}
